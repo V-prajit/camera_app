db2adf3abc855b99383d6623b18a3d9b4e0a9e69
Prajit Viswanadha <83452847+V-prajit@users.noreply.github.com>
2023-10-10 22:00:18 -0500

    bruh

--- main.py
+++ main.py
@@ -1,32 +1,61 @@
 import tkinter as tk
 from tkinter import ttk
-import imageio
+import subprocess
 from PIL import Image, ImageTk
-app = tk.Tk()
-app.title("The Camera App")
+from threading import Thread
 
-#Get frames from the webcam
-def get_frame():
-    input = imageio.get_reader("<video0", format="ffmpeg", mode = "I", input_params=["-r", "30"])
+class CameraApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Camera App")
+        self.label = ttk.Label(root)
+        self.label.pack(padx=10, pady=10)
+        self.is_recording = False
 
-    for frame in input:
-        img = Image.fromarray(frame)
-        img = ImageTk.PhotoImage(img)
-        label.config(image=img)
-        label.image = img
+        self.start_button = ttk.Button(root, text="Start Recording", command=self.start_recording)
+        self.start_button.pack(padx=10, pady=10)
+        self.stop_button = ttk.Button(root, text="Stop Recording", command=self.stop_recording)
+        self.stop_button.pack(padx=10, pady=10)
+        self.process = None
 
-        if root.winfo.exists() == 0:
-            break
+    def start_recording(self):
+        if not self.is_recording:
+            self.is_recording = True
+            command = [
+                "ffmpeg",
+                "-f", "avfoundation",
+                "-i", "0",
+                "-pix_fmt", "yuv420p",
+                "-f", "rawvideo",
+                "-"
+            ]
+            self.process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            self.update_gui()
 
-get_frame()
-#Button to toogle screen recordign
-def Toggle_screen_recording():
-    label.config(text = "button Clicked!")
+    def stop_recording(self):
+        self.is_recording = False
+        if self.process:
+            self.process.terminate()
 
-button = tk.Button(app, text = "Click Me!", command = Toggle_screen_recording)
-button.pack()
+    def update_gui(self):
+        if self.is_recording:
+            raw_frame = self.process.stdout.read(640 * 480 * 3)
+            if raw_frame:
+                img = self.convert_raw_frame_to_image(raw_frame)
+                self.label.config(image=img)
+                self.label.image = img
+                self.root.after(10, self.update_gui)  # Update the GUI every 10 milliseconds
+            else:
+                self.process = None
+                self.label.config(image="")
+        else:
+            self.label.config(image="")
 
+    def convert_raw_frame_to_image(self, raw_frame):
+        img = Image.frombytes('RGB', (640, 480), raw_frame)
+        return ImageTk.PhotoImage(img)
 
-
-
-app.mainloop()
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = CameraApp(root)
+    root.mainloop()
