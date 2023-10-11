7b4f6ebeafecf57a57dd474bd3d4b73cad14666f
Prajit Viswanadha <83452847+V-prajit@users.noreply.github.com>
2023-10-10 21:08:54 -0500

    created a basic button fucntion with a fucntion.

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 import tkinter as tk
+
+app = tk.Tk()
+app.title("The Camera App")
+
+label = tk.Label(app, text = "hello World")
+label.pack()
+
+def Toggle_screen_recording():
+    label.config(text = "button Clicked!")
+
+button = tk.Button(app, text = "Click Me!", command = Toggle_screen_recording)
+button.pack()
+
+app.mainloop()
