059986f2434e12faff0b1e67d97ac19281f9dc03
Prajit Viswanadha <83452847+V-prajit@users.noreply.github.com>
2023-10-10 21:23:25 -0500

    adding the showing of the frames, upon the openign of hte app

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
 import tkinter as tk
-
+from tkinter import ttk
+import imageio
+from PIL import Image, ImageTk
 app = tk.Tk()
 app.title("The Camera App")
 
-label = tk.Label(app, text = "hello World")
-label.pack()
+#Get frames from the webcam
+def get_frame():
+    input = imageio.get_reader("<video0", format="ffmpeg", mode = "I", input_params=["-r", "30"])
 
+    for frame in input:
+        img = Image.fromarray(frame)
+        img = ImageTk.PhotoImage(img)
+        label.config(image=img)
+        label.image = img
+
+        if root.winfo.exists() == 0:
+            break
+
+get_frame()
+#Button to toogle screen recordign
 def Toggle_screen_recording():
     label.config(text = "button Clicked!")
 
 button = tk.Button(app, text = "Click Me!", command = Toggle_screen_recording)
 button.pack()
+
+
+
 
 app.mainloop()
