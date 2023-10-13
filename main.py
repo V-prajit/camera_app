b4c55b85b714281066383af27925be1e9eda86cc
Prajit Viswanadha <83452847+V-prajit@users.noreply.github.com>
2023-10-13 01:40:21 -0500

    fix and optimise

--- main.py
+++ main.py
@@ -2,52 +2,52 @@ from tkinter import *
 import cv2
 from PIL import Image, ImageTk
 
-#set the camera and its values up
+# set the camera and its values up
 cam = cv2.VideoCapture(0)
 frame_width = int(cam.get(3))
 frame_height = int(cam.get(4))
 framerate = int(cam.get(5))
 
-#setting the base window
+# setting the base window
 app = Tk()
 app.bind('<Escape>', lambda e: app.quit())
 
-#place the video feed into tkinter video
+# place the video feed into tkinter video
 vid_src = Label(app)
 vid_src.pack()
 
 global recording_state
 recording_state = False
-out = cv2.VideoWriter('outpy.avi',cv2.VideoWriter_fourcc('M','J','P','G'), framerate, (frame_width,frame_height))
+out = None
 
-#functions to perform actions
-#open the camera preview upon opeing the application
+# functions to perform actions
+# open the camera preview upon opening the application
 def open_camera():
+    global recording_state, out
     _, frame = cam.read()
     c_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
     cur_frame = Image.fromarray(c_image)
-    photo_cur_frame = ImageTk.PhotoImage(image = cur_frame)
+    photo_cur_frame = ImageTk.PhotoImage(image=cur_frame)
     vid_src.photo_image = photo_cur_frame
-    vid_src.configure(image = photo_cur_frame)
+    vid_src.configure(image=photo_cur_frame)
+
+    if recording_state:
+        if out is None:
+            out = cv2.VideoWriter('outpy.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), framerate, (frame_width, frame_height))
+        out.write(frame)
     vid_src.after(10, open_camera)
 
-#state of the recording button, and having it start and stop writing to a file
-def state1():
+# state of the recording button, and having it start and stop writing to a file
+def toggle_recording():
     global recording_state
-    recording_state = True
-    rec_button.configure(text = "Stop Recording", command = state2)
-def state2():
-    global recording_state
-    recording_state = False
-    rec_button.configure(text = "Start Recording", command = state1)
-if recording_state == True:
-    out.write(cam.read())
-out.release()
-rec_button = Button(app, text = 'Start Recording', command = state2)
+    recording_state = not recording_state
+    if recording_state:
+        rec_button.configure(text="Stop Recording")
+    else:
+        rec_button.configure(text="Start Recording")
+
+rec_button = Button(app, text='Start Recording', command=toggle_recording)
 rec_button.pack()
+
 app.after(1, open_camera)
-
 app.mainloop()
-
-
-
