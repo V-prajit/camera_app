1608b3fc1cdd95d8c08da5084798d8512bb0e04d
Prajit Viswanadha <83452847+V-prajit@users.noreply.github.com>
2023-10-26 19:47:06 -0500

    wokr on it

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ frame_width = int(cam.get(3))
 frame_height = int(cam.get(4))
 framerate = 60  # Set the frame rate for recording and playback (in frames per second)
 
+# Initialize video writer
+out = cv2.VideoWriter('outpy.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), framerate, (frame_width, frame_height))
+
 # setting the base window
 app = Tk()
 app.bind('<Escape>', lambda e: app.quit())
@@ -20,18 +23,20 @@ vid_src.pack()
 frame_count_label = Label(app, text="Frame: 0")
 frame_count_label.pack()
 
-rgb_label = Label(app, text = "RGB: ")
+rgb_label = Label(app, text="RGB: ")
 rgb_label.pack()
 
-global recording_state, paused
+global recording_state, paused, next_or_prev_frame
 recording_state = False
 paused = False
-out = None
+next_or_prev_frame = None
+
+total_frames = 0  # Variable to keep track of the total frames recorded
 
 # functions to perform actions
 # open the camera preview upon opening the application
 def open_camera():
-    global recording_state, out, paused
+    global recording_state, paused, total_frames
     _, frame = cam.read()
     frame = cv2.resize(frame, (frame_width, frame_height))
 
@@ -44,9 +49,8 @@ def open_camera():
     vid_src.bind("<Motion>", get_rgb_values)
 
     if recording_state and not paused:
-        if out is None:
-            out = cv2.VideoWriter('outpy.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), framerate, (frame_width, frame_height))
-        out.write(frame)
+        out.write(frame)  # Write frame to video file
+        total_frames += 1  # Increase the total frames recorded
     vid_src.after(1000 // framerate, open_camera)  # Use the specified frame rate for capturing frames
 
 def get_rgb_values(event):
@@ -55,21 +59,22 @@ def get_rgb_values(event):
     rgb = frame[y, x]  # Get the RGB values at the cursor position
     rgb_label.config(text="RGB: {}".format(rgb))
 
-next_or_prev_frame = None  # New global variable to track the operation
-
-
 def play_video():
-    global paused, next_or_prev_frame
-    global cap, current_frame
+    global paused, next_or_prev_frame, total_frames
+    global current_frame
     current_frame = 0
+
     cap = cv2.VideoCapture('outpy.avi')
 
     while True:
         if not paused or (paused and next_or_prev_frame is not None):
             if next_or_prev_frame == "next":
                 current_frame += 1
-            elif next_or_prev_frame == "prev" and current_frame >0:
+            elif next_or_prev_frame == "prev":
                 current_frame -= 1
+
+            # Ensure the current_frame stays within the valid range
+            current_frame = max(0, min(total_frames - 1, current_frame))
 
             cap.set(cv2.CAP_PROP_POS_FRAMES, current_frame)
             ret, frame = cap.read()
@@ -80,14 +85,16 @@ def play_video():
             frame_count_label.config(text="Frame: {}".format(current_frame))
 
             next_or_prev_frame = None
-        
+
         cv2.imshow('Recorded Video', frame)
 
-        if cv2.waitKey(1000 // framerate) & 0xFF == ord('q'):  
+        if cv2.waitKey(1000 // framerate) & 0xFF == ord('q'):
             break
-        
+
     cap.release()
     cv2.destroyAllWindows()
+
+# ... (rest of your code remains the same)
 
 def pause_resume_video():
     global paused
@@ -96,6 +103,7 @@ def pause_resume_video():
 def next_frame():
     global next_or_prev_frame
     next_or_prev_frame = "next"
+
 def prev_frame():
     global next_or_prev_frame
     next_or_prev_frame = "prev"
@@ -118,12 +126,12 @@ play_button.pack()
 pause_resume_button = Button(app, text='Pause/Resume Video', command=pause_resume_video)
 pause_resume_button.pack()
 
-forward_button = Button(app, text='Move Forward', command= next_frame)
+forward_button = Button(app, text='Move Forward', command=next_frame)
 forward_button.pack()
 
-backward_button = Button(app, text='Move Backward', command= prev_frame)
+backward_button = Button(app, text='Move Backward', command=prev_frame)
 backward_button.pack()
 
-#random comment 
+# random comment
 app.after(1, open_camera)
 app.mainloop()
