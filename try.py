d8a04d56d09061d5cce648b3a337a797d1deb370
Prajit Viswanadha <83452847+V-prajit@users.noreply.github.com>
2023-10-31 18:06:08 -0500

    got everything working.

--- try.py
+++ try.py
@@ -1,6 +1,7 @@
 from tkinter import *
 import cv2
 from PIL import Image, ImageTk
+import os
 
 app = Tk()
 app.bind('<Escape>', lambda e: app.quit())
@@ -10,7 +11,7 @@ cam = cv2.VideoCapture(0)
 cam_width = int(cam.get(3))
 cam_height = int(cam.get(4))
 framerate = 60
-write_video = cv2.VideoWriter("output.avi", cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), framerate, (cam_width, cam_height))
+write_video = None
 current_frame_num = 0
 
 recording_state = False
@@ -27,13 +28,11 @@ controls_frame = Frame(app)
 controls_frame.pack(pady=10)
 
 frame_counter_label = Label(app, text="Frame: 0")
-
-
 rgb_label = Label(app)
 
-def show_frame(frame, increment=True):
+def show_frame(frame):
     global current_frame_num
-    if not live_preview and increment:
+    if not live_preview:
         current_frame_num += 1
     frame_counter_label.config(text=f"Frame: {current_frame_num}")
     convert_color = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
@@ -54,52 +53,28 @@ def move_frame_forward():
     global vid_frame, current_frame_num, cap
     if cap is not None and is_paused:
         ret, vid_frame = cap.read()
-        print("Current frame before decrement:", current_frame_num)
-
         if not ret:
-            reset_to_start()
             return
         show_frame(vid_frame)
 
-
 def move_frame_backward():
     global vid_frame, current_frame_num, cap
+    frame_that_is_playing = int(cap.get(cv2.CAP_PROP_POS_FRAMES))
     if cap is not None and is_paused:
-        # Go one frame back
-        if current_frame_num > 0:
-            current_frame_num -= 1
-            cap.set(cv2.CAP_PROP_POS_FRAMES, current_frame_num)
-            ret, vid_frame = cap.read()
+        if frame_that_is_playing > 0:
+            frame_that_is_playing -= 2
+            cap.set(cv2.CAP_PROP_POS_FRAMES, frame_that_is_playing) 
+            ret, vid_frame = cap.read()  
             if ret:
-                show_frame(vid_frame, increment=False)
+                current_frame_num -= 2
+                show_frame(vid_frame)
             else:
                 print("Error reading frame")
         else:
             print("Reached the beginning of the video")
+    else:
+        print("the video is not found")
 
-
-
-
-
-
-def reset_to_start():
-    global recording_state, is_paused, live_preview, vid_frame, frame_cache, cap, current_frame_num
-    current_frame_num = 0
-    frame_counter_label.config(text=f"Frame: {current_frame_num}")
-    recording_state = False
-    is_paused = False
-    live_preview = True
-    vid_frame = None
-    frame_cache = []
-    rgb_label.pack_forget()
-    controls_frame.pack_forget()
-    recording_button.pack(side=LEFT, padx=5)
-    play_button.pack(side=LEFT, padx=5)
-    if cap is not None:
-        cap.release()
-        cap = None
-
-# Rest of the functions
 def preview_window():
     global live_preview
     _, frame = cam.read()
@@ -115,7 +90,6 @@ def play_frame():
     if cap is not None and not is_paused:
         ret, vid_frame = cap.read()
         if ret:
-            frame_cache.append(vid_frame)
             show_frame(vid_frame)
             app.after(1000 // framerate, play_frame)
         else:
@@ -132,11 +106,16 @@ def pause_playback():
         play_frame()
 
 def play_video():
-    global cap, recording_state, live_preview, frame_cache
+    global cap, recording_state, live_preview, frame_cache, write_video
     if cam.isOpened():
         cam.release()
+    if write_video and write_video.isOpened():
+        write_video.release()
 
-    cap = cv2.VideoCapture('output.avi')
+    if not os.path.exists('output.mp4'):
+        print("Error: output.mp4 doesn't exist!")
+        return
+    cap = cv2.VideoCapture('output.mp4')
     recording_state = False
     live_preview = False
     frame_cache = []
@@ -145,7 +124,7 @@ def play_video():
     play_button.pack_forget()
     rgb_label.pack()
     frame_counter_label.pack()
-    
+
     # Pack the video control buttons here
     pause_button.pack(side=LEFT, padx=5)
     move_forward_button.pack(side=LEFT, padx=5)
@@ -154,14 +133,15 @@ def play_video():
     controls_frame.pack(pady=10)  # Ensure controls frame is packed after its content
     play_frame()
 
-
 def toggle_recording():
-    global recording_state
+    global recording_state, write_video
     recording_state = not recording_state
     if recording_state:
         recording_button.config(text="Stop Recording")
+        write_video = cv2.VideoWriter("output.mp4", cv2.VideoWriter_fourcc('m', 'p', '4', 'v'), framerate, (cam_width, cam_height))
     else:
         recording_button.config(text="Start Recording")
+        write_video.release()
 
 video_source.bind('<Motion>', lambda e: update_rgb_values(e.x, e.y))
 
