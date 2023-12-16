f2708c8e5b2cdf1cf3427fa2c6c50f4c0f796302
Prajit Viswanadha <83452847+V-prajit@users.noreply.github.com>
2023-12-16 13:38:56 -0800

    minor_chages

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -1,7 +1,6 @@
-
 #!/bin/bash
 
-# Check if Python 3.9 is installed
+# Check if Python 3.11 is installed
 if ! command -v python3.11 &> /dev/null; then
     echo "Python 3.11 is not installed. Installing Python 3.11..."
 
@@ -20,15 +19,18 @@ else
     echo "Python 3.11 is already installed."
 fi
 
-sudo apt-get Update
+sudo apt-get update
 sudo apt-get install python3-pip
-echo "PIP has been updated"
+if [ $? -ne 0 ]; then
+    echo "Failed to upgrade Pillow."
+    exit 1
+fi
 
 # Check and install Tkinter if necessary
 python3.11 -m tkinter --version &> /dev/null
 if [ $? -ne 0 ]; then
     echo "Tkinter is not installed. Installing Tkinter..."
-   sudo apt-get install python-tk 
+    sudo apt-get install python-tk 
     if [ $? -ne 0 ]; then
         echo "Failed to install Tkinter."
         exit 1
@@ -39,20 +41,26 @@ fi
 
 # Install additional Python packages
 echo "Installing additional Python packages..."
-pip3.11 install opencv-python mediapipe Pillow numpy
+python3.11 -m pip install opencv-python mediapipe Pillow numpy
 if [ $? -ne 0 ]; then
     echo "Failed to install one or more Python packages."
     exit 1
 fi
 
 echo "Upgrading Pillow..."
-sudo python3.9 -m pip install Pillow --upgrade
+sudo python3.11 -m pip install Pillow --upgrade
 if [ $? -ne 0 ]; then
     echo "Failed to upgrade Pillow."
     exit 1
 fi
 
+sudo apt install python3-tk
+if [$? -ne 0 ]; then
+	echo "Failed to install Tkinter"
+	exit 1
+fi
+
+
 echo "All required Python packages have been successfully installed."
 
-echo "Try recording your first video by running 'python3.9 record_video.py'"
-
+echo "Try recording your first video by running 'python3.11 record_video.py'"
